da1a35c321e4e40e33e0f842288f81977fd0b616
wbx <zhongdaima@163.com>
2026-05-08 09:33:55 +0800

    Add files via upload

---
+++ flutter_inappwebview_ios/ios/Classes/WKWebView+SchemeHandle.h
@@ -0,0 +1,16 @@
+//
+//  WKWebView+SchemeHandle.h
+//  yjtProject
+//
+//  Created by wbx on 2023/2/13.
+//
+
+#import <WebKit/WebKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WKWebView (SchemeHandle)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ flutter_inappwebview_ios/ios/Classes/WKWebView+SchemeHandle.m
@@ -0,0 +1,31 @@
+//
+//  WKWebView+SchemeHandle.m
+//  yjtProject
+//
+//  Created by wbx on 2023/2/13.
+//
+
+#import "WKWebView+SchemeHandle.h"
+#import <objc/runtime.h>
+
+@implementation WKWebView (SchemeHandle)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Method originalMethod = class_getClassMethod(self, @selector(handlesURLScheme:));
+        Method swizzledMethod = class_getClassMethod(self, @selector(uxin_handlesURLScheme:));
+        method_exchangeImplementations(originalMethod, swizzledMethod);
+    });
+}
+
++ (BOOL)uxin_handlesURLScheme:(NSString *)urlScheme {
+    if ([urlScheme isEqualToString:@"http"] ||
+        [urlScheme isEqualToString:@"https"]) { // || [urlScheme isEqualToString:@"file"]
+        return  NO;
+    } else {
+        return [self handlesURLScheme:urlScheme];
+    }
+}
+
+@end
